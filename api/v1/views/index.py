0d044179b9983ede595e0c8268a885b43b339ed3
James Alhassan <jamesalhassan052@gmail.com>
2023-04-30 17:17:47 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 ''' Route that returns JSON ok status'''
 from api.v1.views import app_views
+from flask import Flask, jsonify
+
 
 @app_views.route('/status', strict_slashes=False)
 def okStatus():
