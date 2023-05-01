424363837e2c95e4bc1e2cf52e244b149dc7d0ef
James Alhassan <jamesalhassan052@gmail.com>
2023-05-01 16:49:23 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -19,6 +19,13 @@ def closeStorage(ob):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
-    app.run(host, int(port), threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+
+    if not host:
+        host = '0.0.0.0'
+
+    if not port:
+        port = '5000'
+
+    app.run(host=host, port=port, threaded=True)
