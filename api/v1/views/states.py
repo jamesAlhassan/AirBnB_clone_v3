b08d7fdd60e8e8787dbb73b6229b48bb1b6f94b3
James Alhassan <jamesalhassan052@gmail.com>
2023-05-01 21:33:30 +0000

    Update state.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 '''states view module'''
-from flask import jsonify
+from flask import jsonify, abort
 from flasgger.utils import swag_from
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get.yml', methods=['GET'])
+@swag_from('resources/state/get.yml', methods=['GET'])
 def get_all_states():
     """ Get all states """
     all_states = [ob.to_dict() for ob in storage.all(State).values()]
     return jsonify(all_states)
+
+@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
+@swag_from('resources/state/getIid.yml', methods=['GET'])
+def getStateId(state_id):
+    """ Get state by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
