18577c4c7446b7632c3fe64b9e41e8185e49a22e
James Alhassan <jamesalhassan052@gmail.com>
2023-05-01 21:45:28 +0000

    Update state.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 '''states view module'''
 from flask import jsonify, abort
+from api.v1.views import app_views
 from flasgger.utils import swag_from
+from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -21,3 +24,16 @@ def getStateId(state_id):
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('resources/state/delete.yml', methods=['DELETE'])
+def deleteState(state_id):
+    """ Delete state by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    storage.save()
+    return jsonify({})
