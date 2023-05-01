02a119d4002813cedf5a023ad854273af3cd90c7
James Alhassan <jamesalhassan052@gmail.com>
2023-05-01 20:10:17 +0000

    Update views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 ''' Route that returns JSON ok status'''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -16,3 +17,9 @@ def noOfObject():
     """
     Retrieves the number of each objects by type
     """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
