aa0e225dbe28ff46a05adcc740ba749d79788b1f
James Alhassan <jamesalhassan052@gmail.com>
2023-05-02 04:55:26 +0000

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,3 +7,11 @@ from api.v1.views import app_views
 from flasgger.utils import swag_from
 from models import storage
 from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@swag_from('resources/user/get.yml', methods=['GET'])
+def getUsers():
+    """ Get users"""
+    all_users = [obj.to_dict() for obj in storage.all(User).values()]
+    return jsonify(all_list)
