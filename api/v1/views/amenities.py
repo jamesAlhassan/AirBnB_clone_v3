36fbe3c9004d0b89bbd10378bcf182340d17393c
James Alhassan <jamesalhassan052@gmail.com>
2023-05-02 04:20:46 +0000

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,3 +7,11 @@ from flasgger.utils import swag_from
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+@swag_from('resources/amenity/get.yml', methods=['GET'])
+def getAmenities():
+    """ Get amenities by id """
+    all_list = [obj.to_dict() for obj in storage.all(Amenity).values()]
+    return jsonify(all_list)
