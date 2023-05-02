6dc6098cf9f4559e75bf4c4a3c7b78606a5abe87
James Alhassan <jamesalhassan052@gmail.com>
2023-05-02 03:43:50 +0000

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,4 +7,13 @@ from models import storage
 from models.state import State
 from models.city import City
 
-
+@app_views.route('/states/<string:state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
+@swag_from('resources/city/get.yml', methods=['GET'])
+def getCity(state_id):
+    """ Retrieves the list of all City objects of a State """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    list_cities = [obj.to_dict() for obj in state.cities]
+    return jsonify(list_cities)
