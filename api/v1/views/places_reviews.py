97913eb9ccc33937fe48afdbe78cc8bee3f510c7
James Alhassan <jamesalhassan052@gmail.com>
2023-05-03 04:58:15 +0000

    Update reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -9,3 +9,15 @@ from models.place import Place
 from models.review import Review
 from models.user import User
 from flasgger.utils import swag_from
+
+
+@app_views.route('/places/<string:place_id>/reviews',
+                 methods=['GET'], strict_slashes=False)
+@swag_from('resources/reviews/get.yml', methods=['GET'])
+def getReviews(place_id):
+    """ Get reviews  """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    reviews = [obj.to_dict() for obj in place.reviews]
+    return jsonify(reviews)
